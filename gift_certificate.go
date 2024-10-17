d7421b41d7de5bfcf30e2b539cf9440ea7ace12f
Marco Valenziano <mvalenziano@kilkennygroup.com>
2024-10-17 16:55:43 +0200

    fix

--- gift_certificate.go
+++ gift_certificate.go
@@ -4,34 +4,35 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 // map gift certificate structs
 type GiftCertificate struct {
-    ID           int       `json:"id"`
-    Code         string    `json:"code"`
-    Amount       string    `json:"amount"`
-    Status       string    `json:"status"`
-    Balance      string    `json:"balance"`
-    ToName       string    `json:"to_name"`
-    OrderID      int       `json:"order_id"`
-    Template     string    `json:"template"`
-    Message      string    `json:"message"`
-    ToEmail      string    `json:"to_email"`
-    FromName     string    `json:"from_name"`
-    FromEmail    string    `json:"from_email"`
-    CustomerID   int       `json:"customer_id"`
-    ExpiryDate   time.Time    `json:"expiry_date"`   // could be time.Time if parsed
-    PurchaseDate time.Time    `json:"purchase_date"` // could be time.Time if parsed
-    CurrencyCode string    `json:"currency_code"`
+	ID           int       `json:"id,omitempty"`
+	Code         string    `json:"code"`
+	Amount       string    `json:"amount"`
+	Status       string    `json:"status"`
+	Balance      string    `json:"balance"`
+	ToName       string    `json:"to_name,omitempty"`
+	OrderID      int       `json:"order_id"`
+	Template     string    `json:"template,omitempty"`
+	Message      string    `json:"message"`
+	ToEmail      string    `json:"to_email,omitempty"`
+	FromName     string    `json:"from_name,omitempty"`
+	FromEmail    string    `json:"from_email,omitempty"`
+	CustomerID   int       `json:"customer_id,omitempty"`
+	ExpiryDate   time.Time `json:"expiry_date"`   // could be time.Time if parsed
+	PurchaseDate time.Time `json:"purchase_date"` // could be time.Time if parsed
+	CurrencyCode string    `json:"currency_code"`
 }
 
 // GetCart gets a cart by ID from BigCommerce and returns it
 func (bc *Client) GetGiftCertificateByCode(code string) (*GiftCertificate, error) {
 	url := fmt.Sprintf("/v2/gift_certificates?code=%s", code)
-	
+
 	req := bc.getAPIRequest(http.MethodGet, url, nil)
 	res, err := bc.HTTPClient.Do(req)
 	if err != nil {
@@ -59,6 +60,9 @@ func (bc *Client) GetGiftCertificateByCode(code string) (*GiftCertificate, error
 // create a gift certificate
 func (bc *Client) CreateGiftCertificate(giftCertificate *GiftCertificate) (*GiftCertificate, error) {
 	body, _ := json.Marshal(giftCertificate)
+
+	log.Println("Creating gift certificate with body: ", string(body))
+
 	req := bc.getAPIRequest(http.MethodPost, "/v2/gift_certificates", bytes.NewReader(body))
 	res, err := bc.HTTPClient.Do(req)
 	if err != nil {
